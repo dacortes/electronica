e4549cfd32fbddb2133e1e846efee4a09244fe1c
Your Name <you@example.com>
2026-03-31 15:26:56 +0200

    fix: logic button

--- ex02/src/main.c
+++ ex02/src/main.c
@@ -1,25 +1,21 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+// ouput: pin 8
+// input: pin 3
+
 int main(void)
 {
-    DDRB |= (1 << DDB0);
+	DDRB |= (1 << DDB0);
 
-    DDRD &= ~(1 << DDD3);
-    PORTD |= (1 << PORTD3);
-    
-    while(1)
-    {
-		PORTB &= ~(1 << PORTB0); 
-        if (!(PIND & (1 << PIND3)))
-        {
-            PORTB |= (1 << PORTB0);
-        }
-        else
-        {
-            PORTB &= ~(1 << PORTB0);
-        }
-    }
-    
-    return 0;
+	DDRD &= ~(1 << DDD3);
+	PORTD |= (1 << PORTD3);
+	
+	while(1) {
+		if ((PIND & (1 << PIND3)))
+			PORTB |= (1 << PORTB0);
+		else
+			PORTB &= ~(1 << PORTB0);
+	}
+	return 0;
 }
